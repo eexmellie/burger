e5fa64abf9b277b1f7a7df268537ccd33996f59a
Olga Kozlovich <eexmellie@gmail.com>
2017-07-30 14:36:11 +0300

    added gulp-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -175,3 +175,8 @@ gulp.task('default', () => {
     runSequence(['clean', 'wiredep'], 'build', resolve);
   });
 });
+
+gulp.task('deploy', ['default'], () => {
+  return gulp.src('dist/**/*')
+    .pipe($.ghPages());
+});
